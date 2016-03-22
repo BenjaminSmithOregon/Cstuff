3f2cb330c6de0aabc41c80ce5752456e6cb644b7
[BenjaminSmithOregon] <[benjamin.smith.oregon@gmail.com]>
2016-03-22 15:32:14 -0700

    Adding the first tutorial to C repo

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Hello World.");
+  printf("This is the second line in my file...");
+  printf("...how about another line of code!");
+  printf("OK, this is the last line of code I promise :)");
+
+  return 0;
+}
